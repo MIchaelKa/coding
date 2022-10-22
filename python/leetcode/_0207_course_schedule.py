9c10afb34ad0223d49dcad0f4be0f5cbc09a71a8
michaelka <michael.kalinin.nn@gmail.com>
2022-10-22 17:36:17 +0300

    [leetcode] 207. Course Schedule. Improve solution.

--- python/leetcode/_0207_course_schedule.py
+++ python/leetcode/_0207_course_schedule.py
@@ -1,34 +1,46 @@
 from __future__ import annotations
 
 class GraphSolver:
+
+    def __init__(self):
+        super().__init__()
+        self.verbose = False
         
     def canFinish(self, numCourses: int, prerequisites: list[list[int]]) -> bool:
 
         self.discovered = {}
         self.processed = {}
 
+        # После введения этой дополнительной структуры значительно выиграли в скорости
+        # и при этом не сильно проиграли в памяти.
+        # Почему не сильно проиграли в памяти?
+        self.graph = [[] for _ in range(numCourses)]
+
+        for i, j in prerequisites:
+             self.graph[i].append(j)
+
         for i in range(numCourses):
             if i not in self.discovered:
-                # print(f"component: {i}")
-                if not self.canFinishHelper(i, prerequisites):
+                if self.verbose:
+                    print(f"component: {i}")
+                if not self.canFinishHelper(i):
                     return False
         
         return True
 
-    def canFinishHelper(self, v: int, prerequisites: list[list[int]]) -> bool:
+    def canFinishHelper(self, v: int) -> bool:
         
         self.discovered[v] = True
         
-        for p in prerequisites:
-            if p[0] == v:
-                if p[1] not in self.discovered:
-                    if not self.canFinishHelper(p[1], prerequisites):
-                        return False
-                if p[1] not in self.processed:
+        for p in self.graph[v]:
+            if p not in self.discovered:
+                if not self.canFinishHelper(p):
                     return False
-                
+            if p not in self.processed:
+                return False
+
         self.processed[v] = True
-                
+
         return True
 
     
@@ -54,6 +66,8 @@ def run_test():
     prerequisites = [[0,1],[1,2],[2,3],[3,4],[5,6],[6,7],[7,8],[8,6],[7,3]]
     numCourses = 9
     assert(solver.canFinish(numCourses, prerequisites)==False)
+
+    print("test passed")
         
 if __name__ == '__main__':
     run_test()
