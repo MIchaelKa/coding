9507422c3d20a6f9f6a29948d07ac800b0ddc0bd
michaelka <michael.kalinin.nn@gmail.com>
2022-10-25 00:20:08 +0300

    [leetcode] 310. Minimum Height Trees (Draft 3)

--- python/leetcode/_0310_min_height_tree.py
+++ python/leetcode/_0310_min_height_tree.py
@@ -30,7 +30,7 @@ class Solution:
         self.depth = {}
 
         #
-        # Step 1. Calculate depth for all nodes atarting from root
+        # Step 1. Calculate depth for all nodes starting from root
         #
         root = 0
         self.dfs(root)
@@ -67,24 +67,21 @@ class Solution:
                 max_2_depth = child_depth
                 max_2 = child
 
-        if max_1_depth == max_2_depth:
+        diff = max_1_depth - max_2_depth
+        
+        if diff == 0:
             return [v]
         else:
-            
-            if depth == -1:
-                diff = max_1_depth - max_2_depth
-                depth = int(diff / 2)
-                self.more_than_one = (diff % 2 != 0)
-                if self.more_than_one:
-                    depth += 1
 
+            if depth == -1:   
+                depth = int(diff / 2) + int(diff % 2 != 0) # ceil
+   
             nodes = []
-            if self.depth[max_1] > depth:
-                nodes.extend(self.search(max_1, depth-1))
-            if max_2 != -1 and (max_1_depth - max_2_depth == 1) and self.depth[max_2] > depth:
-                nodes.extend(self.search(max_2, depth-1))
 
-            if depth == 1 and self.more_than_one:
+            # if self.depth[max_1] >= depth:
+            nodes.extend(self.search(max_1, depth-1))
+
+            if depth == 1 and diff == 1:
                 nodes.append(v)
 
             return nodes
@@ -109,22 +106,32 @@ def run_tests():
     num_v = 1
     edges = []
     result = solution.findMinHeightTrees(num_v, edges)
-    assert(sorted(result) == [0]) 
+    assert(sorted(result) == [0])
+    
+    num_v = 2
+    edges = [[0,1]]
+    result = solution.findMinHeightTrees(num_v, edges)
+    assert(sorted(result) == [0,1])
 
     num_v = 4
     edges = [[1,0],[1,2],[1,3]]
     result = solution.findMinHeightTrees(num_v, edges)
     assert(sorted(result) == [1]) 
 
+    num_v = 5
+    edges = [[0,1],[0,2],[0,3],[3,4]]
+    result = solution.findMinHeightTrees(num_v, edges)
+    assert(sorted(result) == [0,3])
+
     num_v = 6
     edges = [[3,0],[3,1],[3,2],[3,4],[5,4]]
     result = solution.findMinHeightTrees(num_v, edges)
     assert(sorted(result) == [3,4])
 
-    num_v = 5
-    edges = [[0,1],[0,2],[0,3],[3,4]]
+    num_v = 7
+    edges = [[0,1],[1,2],[1,3],[2,4],[3,5],[4,6]]
     result = solution.findMinHeightTrees(num_v, edges)
-    assert(sorted(result) == [0,3])
+    assert(sorted(result) == [1,2])
 
     num_v = 8
     edges = [[0,1],[1,2],[2,3],[3,4],[4,5],[5,6],[5,7]]
@@ -136,11 +143,6 @@ def run_tests():
     result = solution.findMinHeightTrees(num_v, edges)
     assert(sorted(result) == [5])
 
-    num_v = 7
-    edges = [[0,1],[1,2],[1,3],[2,4],[3,5],[4,6]]
-    result = solution.findMinHeightTrees(num_v, edges)
-    assert(sorted(result) == [2,3])
-
     print("test passed")
 
 
@@ -150,8 +152,8 @@ if __name__ == '__main__':
 
     solution = Solution()
 
-    num_v = 10
-    edges = [[0,1],[0,2],[0,3],[2,4],[0,5],[5,6],[6,7],[2,8],[7,9]]
+    num_v = 11
+    edges = [[0,1],[0,2],[2,3],[0,4],[2,5],[5,6],[3,7],[6,8],[8,9],[9,10]]
     result = solution.findMinHeightTrees(num_v, edges)
     print(result)
 
