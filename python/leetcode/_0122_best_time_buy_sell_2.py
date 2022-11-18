e52320240e209b02719e1208fbfce4e1e820cbbc
michaelka <michael.kalinin.nn@gmail.com>
2022-11-18 20:21:57 +0300

    [leetcode] 122. Best Time to Buy and Sell Stock II

---
+++ python/leetcode/_0122_best_time_buy_sell_2.py
@@ -0,0 +1,59 @@
+"""
+[leetcode] 122. Best Time to Buy and Sell Stock II
+
+You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+On each day, you may decide to buy and/or sell the stock.
+You can only hold at most one share of the stock at any time.
+However, you can buy it then immediately sell it on the same day.
+
+Find and return the maximum profit you can achieve.
+
+"""
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        total = 0
+        last_min = prices[0]
+        last_diff = 0
+
+        for i in range(1, len(prices)):
+            if prices[i] < last_min:
+                total += last_diff
+                last_min = prices[i]
+                last_diff = 0
+                continue
+
+            new_diff = prices[i]-last_min
+            if new_diff > last_diff:
+                last_diff = new_diff
+            else:
+                total += last_diff
+                last_min = prices[i]
+                last_diff = 0
+                continue
+        
+        return total + last_diff
+
+
+def run_tests(solution):
+
+    prices = [7,1,5,3,6,4]
+    result = solution.maxProfit(prices)
+    assert(result==7)
+    
+    prices = [1,2,3,4,5]
+    result = solution.maxProfit(prices)
+    assert(result==4)
+
+    print("Tests passed!")
+
+if __name__ == '__main__':
+
+    solution = Solution()
+    run_tests(solution)
+
+    prices = [7,1,5,3,6,4]
+    result = solution.maxProfit(prices)
+    print(result)
