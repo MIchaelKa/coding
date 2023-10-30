622a745a52f7634e40337ae5ae882b58b497b695
michaelka <michael.kalinin.nn@gmail.com>
2023-10-30 21:10:51 +0100

    [leetcode] 238. Shorter arrays

--- python/leetcode/_0238_product_exept_self.py
+++ python/leetcode/_0238_product_exept_self.py
@@ -7,7 +7,11 @@ Tags:
 #array
 
 Takeaways:
-- append can take too much time, better to allocate memory in advance
+- append() can take too much time, better to allocate memory in advance
+
+TODO:
+- do not use extra memory for left_prod and right_prod
+- is it possible to implement cumprod() more efficiently?
 
 '''
 
@@ -24,10 +28,23 @@ class Solution:
         for i in range(1, len(nums)):
             right_prod[i] = right_prod[i-1] * nums[-i]
 
-        print(left_prod)
-        print(right_prod)
-
         answer = [left_prod[i] * right_prod[-i-1] for i in range(len(nums))]
+
+        return answer
+
+class Solution_2:
+
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # Shorter arrays
+        left_prod = [1] * (len(nums)-1)
+        for i in range(0, len(left_prod)):
+            left_prod[i] = left_prod[i-1] * nums[i]
+
+        right_prod = [1] * (len(nums)-1)
+        for i in range(0, len(right_prod)):
+            right_prod[i] = right_prod[i-1] * nums[-i-1]
+
+        answer = [] # TODO: but how to count the answer now?
 
         return answer
 
