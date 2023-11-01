b471f7b4abdfc936df3d370bce1f7b1be5fa407f
michaelka <michael.kalinin.nn@gmail.com>
2023-11-01 14:36:59 +0100

    [leetcode] 128. Longest Consecutive Sequence. Still wrong

--- python/leetcode/_0128_longest_consecutive_sequence.py
+++ python/leetcode/_0128_longest_consecutive_sequence.py
@@ -13,74 +13,100 @@ from typing import List
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
 
-        # (start, end) -> len
-        # num -> (len, num)
-
         counter = {}
 
         for n in nums:
+
+            # print(n)
 
             if n-1 in counter and n+1 in counter:
                 left = counter.pop(n-1)
                 right = counter.pop(n+1)
 
-                new_len = left[0] + right[0] + 1
-
-                if left[1] is not None and right[1] is not None:
-                    counter[left[1]] = (new_len, right[1])
-                    counter[right[1]] = (new_len, left[1])
-                elif left[1] is not None:
-                    counter[left[1]] = (new_len, n+1)
-                    counter[n+1] = (new_len, left[1])
-                elif right[1] is not None:
-                    counter[n-1] = (new_len, right[1])
-                    counter[right[1]] = (new_len, n-1)
-                else:
-                    counter[n-1] = (new_len, n+1)
-                    counter[n+1] = (new_len, n-1)
+                if left is not None and right is not None:
+                    counter[left] = right
+                    counter[right] = left
+                elif left is not None and left < n:
+                    counter[left] = n+1
+                    counter[n+1] = left
+                elif right is not None and right > n :
+                    counter[n-1] = right
+                    counter[right] = n-1
+                elif left is None and right is None:
+                    counter[n-1] = n+1
+                    counter[n+1] = n-1
 
             elif n-1 in counter:
-                item = counter.pop(n-1)
-                new_len = item[0] + 1
-                item_key = item[1]
+                left = counter[n-1]
+                if left is None:
+                    counter[n-1] = n
+                    counter[n] = n-1
+                elif left < n:
+                    counter[left] = n
+                    counter[n] = left
+                    counter.pop(n-1)
 
-                if item_key is None:
-                    item_key = n-1
-                
-                counter[item_key] = (new_len, n)
-                counter[n] = (new_len, item_key)
             elif n+1 in counter:
-                item = counter.pop(n+1)
-                new_len = item[0] + 1
-                item_key = item[1]
-
-                if item_key is None:
-                    item_key = n+1
-                
-                counter[item_key] = (new_len, n)
-                counter[n] = (new_len, item_key)
+                right = counter[n+1]
+                if right is None:
+                    counter[n+1] = n
+                    counter[n] = n+1
+                elif right > n:
+                    counter[right] = n
+                    counter[n] = right
+                    counter.pop(n+1)
             elif n not in counter:
-                counter[n] = (1, None)
+                counter[n] = None
 
-            print(counter)
+            # print(f'{counter}, len: {len(counter)}')
 
-        print(counter)
-        max = 0
+        print(f'{counter}, len: {len(counter)}')
+
+        max_len = 0
         for k, v in counter.items():
-            if v[0] > max:
-                max = v[0]
+            if v is None:
+                cur_len = 1
+            else:
+                max_elem, min_elem = k, v # if k > v else v, k
+                cur_len = max_elem - min_elem + 1
+                
+            if cur_len > max_len:
+                max_len = cur_len
 
-        return max
+        return max_len
 
+def run_tests(solution):
+
+    nums = [100,4,200,1,3,2]
+    result = solution.longestConsecutive(nums)
+    assert(result==4)
+
+    nums = [2,1,4,3,100,7,15,12,25,9,32,8,10]
+    result = solution.longestConsecutive(nums)
+    assert(result==4)
+
+    nums = [2,1,4,3,100,7,15,12,25,9,32,8,6,10,11]
+    result = solution.longestConsecutive(nums)
+    assert(result==7)
+
+    nums = [-2,-3,-3,7,-3,0,5,0,-8,-4,-1,2]
+    result = solution.longestConsecutive(nums)
+    assert(result==5)
+
+    nums = [-9,-4,9,-7,0,7,3,-1,6,2,-2,8,-2,0,2,-7,-5,-2,6,-5,0,-8,8,1,0,6,8,-8,-1]
+    result = solution.longestConsecutive(nums)
+    assert(result==6)
+
+    print("Tests passed!")
 
 if __name__ == '__main__':
 
     solution = Solution()
 
-    # nums = [100,4,200,1,3,2]
-    # nums = [2,1,4,5,3]
-    # nums = [2,1,4,5,3,100,7,15,12,25,9,32,8,10,11]
-    nums = [1,2,0,1]
+    run_tests(solution)
+
+    nums = [-9,-4,9,-7,0,7,3,-1,6,2,-2,8,-2,0,2,-7,-5,-2,6,-5,0,-8,8,1,0,6,8,-8,-1]
+    # nums = [-2,-1,0,1,2,3,-1]
 
     answer = solution.longestConsecutive(nums)
     print(answer)
