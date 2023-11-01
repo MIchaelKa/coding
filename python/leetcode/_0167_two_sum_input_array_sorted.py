377f770fd48036674fc5dd730c3700b73329af11
michaelka <michael.kalinin.nn@gmail.com>
2023-11-01 20:22:52 +0100

    [leetcode] 167. Two Sum II - Input Array Is Sorted. Solved

---
+++ python/leetcode/_0167_two_sum_input_array_sorted.py
@@ -0,0 +1,40 @@
+'''
+[leetcode] 167. Two Sum II - Input Array Is Sorted
+
+Your solution must use only constant extra space.
+
+_0167_two_sum_input_array_sorted
+
+Tags:
+#array, two_pointers
+
+'''
+
+from typing import List
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+
+        low = 0
+        high = len(numbers) - 1
+        
+        while low < high:
+            guess = numbers[low] + numbers[high]
+            if guess == target:
+                return[low+1, high+1]
+            elif guess > target:
+                high -= 1
+            else:
+                low += 1
+
+        return []
+
+if __name__ == '__main__':
+
+    solution = Solution()
+
+    numbers = [1,2,4,7,11,15]
+    target = 9
+
+    answer = solution.twoSum(numbers, target)
+    print(answer)
