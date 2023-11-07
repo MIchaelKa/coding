fa540d78fa71fa6b597b2aa845573e42e1c793b4
michaelka <michael.kalinin.nn@gmail.com>
2023-11-07 20:58:53 +0100

    less code

--- python/leetcode/_0110_balanced_tree.py
+++ python/leetcode/_0110_balanced_tree.py
@@ -3,6 +3,9 @@
 
 _0110_balanced_tree
 
+Notes:
+Think about implementation of iterative solution.
+
 Tags:
 #tree
 
@@ -10,7 +13,7 @@ Tags:
 
 from common.tree import *
 
-class Solution:
+class Solution_1:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:    
         _, balanced = self.dfs(root)
         return balanced
@@ -35,10 +38,40 @@ class Solution:
         right_h += 1
 
         return max(left_h, right_h), abs(left_h-right_h) <= 1
+    
+class Solution_2:
+    """
+    Solution_2
+    Less code.
+    Is it slower than Solution_1?
+    """
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:    
+        _, balanced = self.dfs(root)
+        return balanced
+    
+    def dfs(self, root: Optional[TreeNode]) -> (int, bool):     
+        if root is None:
+            return 0, True
+            
+        left, right = self.dfs(root.left), self.dfs(root.right)
+        balanced = abs(left[0]-right[0]) <= 1 and left[1] and right[1]
 
+        return max(left[0], right[0]) + 1, balanced
+    
+def run_tests(solution):
+
+    tree = init_tree_from_array([3,1,20,15,70])
+    assert(solution.isBalanced(tree)==True)
+
+    tree = init_tree_from_array([1,3,20,15,70])
+    assert(solution.isBalanced(tree)==False)
+
+    print("tests passed.")
 
 def main():
-    solution = Solution()
+    solution = Solution_2()
+
+    run_tests(solution)
 
     tree = init_tree_from_array([3,1,20,15,70])
     tree.show()
