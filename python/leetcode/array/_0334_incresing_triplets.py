d6bc4730a6b6de129dc5dff724778369d4f4d9c7
michaelka <michael.kalinin.nn@gmail.com>
2024-02-08 12:30:10 +0300

    _0334_incresing_triplets. slow

--- python/leetcode/array/_0334_incresing_triplets.py
+++ python/leetcode/array/_0334_incresing_triplets.py
@@ -10,7 +10,6 @@ from typing import List
 class Solution:
     """
         Solution.
-        TLE
 
         Complexity:
             time: O(n^2)
@@ -19,13 +18,25 @@ class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
 
         cache = [1] * len(nums)
-
+        
         for i in range(1, len(nums)):
+
+            current_max = float('-inf')
+
             for j in range(1, i+1):
-                if nums[i] > nums[i-j]:
-                    cache[i] = max(cache[i], cache[i-j]+1)
+                if nums[i] >= nums[i-j]:
+
+                    if nums[i] > nums[i-j]:
+                        cache[i] = max(cache[i], cache[i-j]+1)
+                    else: # ==
+                        cache[i] = max(cache[i], cache[i-j])
+                
                     if cache[i] == 3:
                         return True
+                    
+                    current_max = max(current_max, nums[i-j])         
+                    if current_max >= nums[i]-1:
+                        break
 
         return False
 
@@ -38,8 +49,10 @@ def main():
 
     run_tests(solution)
 
-    nums = [10,100,12,9,10,2,11,13]
-    nums = [10,100,12,9,10,2,10,9]
+    # nums = [10,100,12,9,10,2,11,13]
+    # nums = [10,100,12,9,10,2,10,9]
+    # nums = [1,1,1,1,1,1,1,1,1,1,1,1,1,3,7]
+    nums = [1,0,-1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2]
 
     print(nums)
     result = solution.increasingTriplet(nums)
