72180148761380b06caacbb8d5def5313af28320
michaelka <michael.kalinin.nn@gmail.com>
2024-06-12 21:39:38 +0400

    _0205_isomorphic_strings

--- cpp/leetcode/_0205_isomorphic_strings.cpp
+++ cpp/leetcode/_0205_isomorphic_strings.cpp
@@ -1,3 +1,8 @@
+/**
+ * _0205_isomorphic_strings
+ * https://leetcode.com/problems/isomorphic-strings
+ * 
+*/
 
 #include <iostream>
 #include <string>
@@ -5,9 +10,35 @@
 using namespace std;
 
 class Solution {
+
+/**
+ * Solution
+ * 
+ * Complexity:
+ *     time: O(n)
+ *     memory: O(n)
+*/
+        
 public:
     bool isIsomorphic(string s, string t) {
 
+        unordered_map<char, char> map_s_t = {};
+        unordered_map<char, char> map_t_s = {};
+
+        for (size_t i = 0; i < s.size(); ++i) {
+            if (map_s_t.find(s[i]) != map_s_t.end()) {
+                if (map_s_t[s[i]] != t[i]) {
+                    return false;
+                }
+            } else if (map_t_s.find(t[i]) != map_t_s.end()) {
+                if (map_t_s[t[i]] != s[i]) {
+                    return false;
+                }
+            } else {
+                map_s_t[s[i]] = t[i];
+                map_t_s[t[i]] = s[i];
+            }
+        }
 
         return true;     
     }
@@ -18,8 +49,8 @@ int main() {
 
     auto solution = make_shared<Solution>();
 
-    string s = "egg";
-    string t = "add";
+    string s = "eggea";
+    string t = "baaba";
 
     cout << "s: " << s << endl;
     cout << "t: " << t << endl;
