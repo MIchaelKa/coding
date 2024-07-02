9b458a5dba7bb05671130cdc79988a6b093883bb
michaelka <michael.kalinin.nn@gmail.com>
2024-07-02 16:57:40 +0400

    _0253_meeting_rooms_2. heap 2

--- python/leetcode/intervals/_0253_meeting_rooms_2.py
+++ python/leetcode/intervals/_0253_meeting_rooms_2.py
@@ -45,7 +45,7 @@ class Solution1:
 
 import heapq
 
-class Solution:
+class Solution2:
     """
         Heap.
 
@@ -63,6 +63,26 @@ class Solution:
                 heapq.heappush(heap, intervals[i][1])
             else:
                 heapq.heappushpop(heap, intervals[i][1])
+
+        return len(heap)
+    
+class Solution:
+    """
+        Heap++ (or -- ?)
+
+        Complexity:
+            time: O(n*log(n))
+            memory: O(n)
+    """
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+
+        intervals.sort() # key=lambda x: x[0]
+        heap = [intervals[0][1]]
+
+        for i in range(1, len(intervals)):
+            if heap[0] <= intervals[i][0]:
+                 heapq.heappop(heap)         
+            heapq.heappush(heap, intervals[i][1])
 
         return len(heap)
 
