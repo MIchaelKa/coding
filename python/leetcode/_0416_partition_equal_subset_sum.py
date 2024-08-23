871c7d9cb184948dac07a2abd48daa9cf6f6c656
michaelka <michael.kalinin.nn@gmail.com>
2024-08-23 23:10:40 +0400

    _0416_partition_equal_subset_sum

--- python/leetcode/_0416_partition_equal_subset_sum.py
+++ python/leetcode/_0416_partition_equal_subset_sum.py
@@ -8,18 +8,20 @@ Related problems:
 TODO:
 
 Tags:
+#backtracking, #dp
 
 """
 
 from typing import List
+from python.common.utils import print_cost_matrix
 
-class Solution:
+class Solution_1:
     """
-        Solution.
+        Backtracking.
 
         Complexity:
-            time: O()
-            memory: O()
+            time: O(2^N)
+            memory: O(N)
     """
     def canPartition(self, nums: List[int]) -> bool:
 
@@ -49,6 +51,41 @@ class Solution:
 
         return False
 
+class Solution:
+    """
+        DP.
+        bottom-up
+
+        Complexity:
+            time: O(N*M)
+            memory: O(N*M)
+    """
+    def canPartition(self, nums: List[int]) -> bool:
+
+        all_sum = sum(nums)
+
+        if all_sum % 2 != 0:
+            return False
+  
+        target_sum = all_sum // 2
+
+        dp = [[False] * (target_sum+1) for _ in range(len(nums)+1)]
+        for i in range(0, len(nums)+1):
+            dp[i][0] = True
+
+        # print_cost_matrix(dp)
+
+        for i in range(1, len(nums)+1):
+            for j in range(1, target_sum+1):
+                prev_target = j-nums[i-1]
+                dp[i][j] = dp[i-1][j] or (dp[i-1][prev_target] if prev_target >= 0 else False)
+
+        # print_cost_matrix(dp)
+        
+        return dp[-1][-1]
+
+
+
 
 
 def run_tests(solution):
@@ -63,8 +100,10 @@ def main():
     nums = [1,5,11,5]
     nums = [1,5,11,5,3]
     nums = [1,2,5]
-    
+
     nums = [1,2,7,3,2,3]
+
+    # nums = [100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,99,97]
 
     print(nums)
     result = solution.canPartition(nums)
