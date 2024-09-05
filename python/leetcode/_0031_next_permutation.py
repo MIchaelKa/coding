164d3883dbbfbb6d22e4ff3db75f9558145a8a55
michaelka <michael.kalinin.nn@gmail.com>
2024-09-05 13:56:06 +0300

    _0031_next_permutation

--- python/leetcode/_0031_next_permutation.py
+++ python/leetcode/_0031_next_permutation.py
@@ -14,13 +14,46 @@ from typing import List
 
 class Solution:
     """
-        Solution.
+        Assume x is in [0,n] and every x in distinct.
+        NW.
 
         Complexity:
             time: O()
             memory: O()
     """
-    pass
+
+    def swap(self, nums: List[int]) -> int:
+        for i in range(len(nums)-1,-1,-1):
+            for j in range(i-1,-1,-1):
+                if nums[i] > nums[j]:
+                    nums[i], nums[j] = nums[j], nums[i]
+                    return j
+
+        return -1
+
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        pivot = self.swap(nums)
+
+        if pivot < 0:
+            low = 0
+            high = len(nums)-1
+            while low < high:
+                nums[low], nums[high] = nums[high], nums[low]
+                low += 1
+                high -= 1
+
+        hashmap = { i+1:1 for i in range(len(nums)) }
+        
+        for i in range(pivot+1):
+            hashmap.pop(nums[i])
+
+        res = list(hashmap.keys())
+        for i in range(len(res)):
+            nums[pivot+1+i] = res[i]
+     
 
 def run_tests(solution):
     print("test passed!")
@@ -31,11 +64,12 @@ def main():
 
     run_tests(solution)
 
-    nums = [8,-19,5,-4,20]
+    nums = [3,2,4,1]
+    nums = [5,4,3,2,1]
 
     print(nums)
-    result = solution.func(nums)
-    print(result)
+    solution.nextPermutation(nums)
+    print(nums)
 
 
 if __name__ == '__main__':
