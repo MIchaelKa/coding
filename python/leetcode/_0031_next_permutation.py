964e2ef832e01f81dcbfa16307e5f90377a4c01a
michaelka <michael.kalinin.nn@gmail.com>
2024-09-05 12:12:00 +0300

    _0560_subarray_sum_equals_k

---
+++ python/leetcode/_0031_next_permutation.py
@@ -0,0 +1,42 @@
+"""
+_0031_next_permutation
+
+Takeaways:
+
+TODO:
+
+Related problems:
+Tags:
+
+"""
+
+from typing import List
+
+class Solution:
+    """
+        Solution.
+
+        Complexity:
+            time: O()
+            memory: O()
+    """
+    pass
+
+def run_tests(solution):
+    print("test passed!")
+
+def main():
+
+    solution = Solution()
+
+    run_tests(solution)
+
+    nums = [8,-19,5,-4,20]
+
+    print(nums)
+    result = solution.func(nums)
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
--- python/leetcode/array/_0560_subarray_sum_equals_k.py
+++ python/leetcode/array/_0560_subarray_sum_equals_k.py
@@ -4,7 +4,7 @@ _0560_subarray_sum_equals_k
 Takeaways:
 
 TODO:
-- Prefix sum + hashmap
+- Prefix sum + hashmap - DONE
 
 Related problems:
 _0238_product_exept_self
@@ -15,6 +15,8 @@ Tags:
 """
 
 from typing import List
+
+from collections import defaultdict
 
 class Solution:
     """
@@ -40,10 +42,24 @@ class Solution:
 
         Complexity:
             time: O(n)
-            memory: O(1)
+            memory: O(n)
     """
     def subarraySum(self, nums: List[int], k: int) -> int:
         result = 0
+
+        cum_sum = []
+        cur_sum = 0
+        for n in nums:
+            cur_sum += n
+            cum_sum.append(cur_sum)
+
+        hashmap = defaultdict(int)
+        result = 0
+
+        for i in range(len(nums)-1,-1,-1):
+            res = k - nums[i]
+            hashmap[cum_sum[i]] += 1
+            result += hashmap[res+cum_sum[i]]
 
         return result
 
@@ -56,11 +72,14 @@ def main():
 
     run_tests(solution)
 
-    nums = [1,2,3]
-    k = 3
+    # nums = [1,2,3]
+    # k = 3
 
     nums = [1,2,3,4,5,1,-5,-4,-3,-2]
     k = 1
+
+    # nums = [1,-1,0]
+    # k = 0
 
     print(nums, k)
     result = solution.subarraySum(nums, k)
